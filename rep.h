2df1194a503c39e7bf7405793cd2c4dc84a586ce
senkin <senkin7853@gmail.com>
2026-04-14 09:36:19 +0800

    fix esp32

--- rep.h
+++ rep.h
@@ -4,49 +4,89 @@
 #include <stddef.h>
 #include <stdint.h>
 
-typedef enum eRepSTM32Platform {
-    REP_STM32_F0 = 1U,
-    REP_STM32_F1 = 2U,
-    REP_STM32_F4 = 3U,
-    REP_STM32_F7 = 4U,
-    REP_STM32_H7 = 5U,
-    REP_STM32_G4 = 6U,
-} eRepSTM32Platform;
+#ifndef REP_STM32_F0
+#define REP_STM32_F0 1U
+#endif
+#ifndef REP_STM32_F1
+#define REP_STM32_F1 2U
+#endif
+#ifndef REP_STM32_F4
+#define REP_STM32_F4 3U
+#endif
+#ifndef REP_STM32_F7
+#define REP_STM32_F7 4U
+#endif
+#ifndef REP_STM32_H7
+#define REP_STM32_H7 5U
+#endif
+#ifndef REP_STM32_G4
+#define REP_STM32_G4 6U
+#endif
 
-typedef enum eRepGd32Platform {
-    REP_GD32F1 = 1U,
-    REP_GD32F3 = 2U,
-    REP_GD32F4 = 3U,
-    REP_GD32F7 = 4U,
-} eRepGd32Platform;
+#ifndef REP_GD32F1
+#define REP_GD32F1 1U
+#endif
+#ifndef REP_GD32F3
+#define REP_GD32F3 2U
+#endif
+#ifndef REP_GD32F4
+#define REP_GD32F4 3U
+#endif
+#ifndef REP_GD32F7
+#define REP_GD32F7 4U
+#endif
 
-typedef enum eRepEsp32Platform {
-    REP_ESP32 = 1U,
-    REP_ESP32C5 = 2U,
-    REP_ESP32S3 = 3U,
-} eRepEsp32Platform;
+#ifndef REP_ESP32
+#define REP_ESP32 1U
+#endif
+#ifndef REP_ESP32C5
+#define REP_ESP32C5 2U
+#endif
+#ifndef REP_ESP32S3
+#define REP_ESP32S3 3U
+#endif
 
-typedef enum Rep_MCU_PLATFORM {
-    REP_MCU_PLATFORM_GD32 = 1U,
-    REP_MCU_PLATFORM_STM32 = 2U,
-    REP_MCU_PLATFORM_ESP32 = 3U,
-} eRepMcuPlatform;
+#ifndef REP_MCU_PLATFORM_GD32
+#define REP_MCU_PLATFORM_GD32 1U
+#endif
+#ifndef REP_MCU_PLATFORM_STM32
+#define REP_MCU_PLATFORM_STM32 2U
+#endif
+#ifndef REP_MCU_PLATFORM_ESP32
+#define REP_MCU_PLATFORM_ESP32 3U
+#endif
 
-typedef enum Rep_RtosSystem {
-    REP_RTOS_NONE = 0U,
-    REP_RTOS_FREERTOS = 1U,
-    REP_RTOS_CUBEMX_FREERTOS = 2U,
-    REP_RTOS_UCOSII = 3U,
-    REP_RTOS_UCOSIII = 4U,
-} eRepRtosSystem;
+#ifndef REP_RTOS_NONE
+#define REP_RTOS_NONE 0U
+#endif
+#ifndef REP_RTOS_FREERTOS
+#define REP_RTOS_FREERTOS 1U
+#endif
+#ifndef REP_RTOS_CUBEMX_FREERTOS
+#define REP_RTOS_CUBEMX_FREERTOS 2U
+#endif
+#ifndef REP_RTOS_UCOSII
+#define REP_RTOS_UCOSII 3U
+#endif
+#ifndef REP_RTOS_UCOSIII
+#define REP_RTOS_UCOSIII 4U
+#endif
 
-typedef enum Rep_LogLevel {
-    REP_LOG_LEVEL_NONE = 0U,
-    REP_LOG_LEVEL_ERROR = 1U,
-    REP_LOG_LEVEL_WARN = 2U,
-    REP_LOG_LEVEL_INFO = 3U,
-    REP_LOG_LEVEL_DEBUG = 4U,
-} eRepLogLevel;
+#ifndef REP_LOG_LEVEL_NONE
+#define REP_LOG_LEVEL_NONE 0U
+#endif
+#ifndef REP_LOG_LEVEL_ERROR
+#define REP_LOG_LEVEL_ERROR 1U
+#endif
+#ifndef REP_LOG_LEVEL_WARN
+#define REP_LOG_LEVEL_WARN 2U
+#endif
+#ifndef REP_LOG_LEVEL_INFO
+#define REP_LOG_LEVEL_INFO 3U
+#endif
+#ifndef REP_LOG_LEVEL_DEBUG
+#define REP_LOG_LEVEL_DEBUG 4U
+#endif
 
 typedef enum eDrvStatus {
 	DRV_STATUS_OK = 0,
